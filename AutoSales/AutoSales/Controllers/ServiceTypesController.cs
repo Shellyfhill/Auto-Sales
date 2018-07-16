04e5f0305ca00cb7eeee88c28875a07c81a9378b
Shellyfhill <shellyfhill@gmail.com>
2018-07-15 20:39:57 -0400

    Added ServiceTypesController and Model

---
+++ AutoSales/AutoSales/Controllers/ServiceTypesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoSales.Data;
+using AutoSales.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoSales.Controllers
+{
+    public class ServiceTypesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ServiceTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_db.ServiceTypes.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ServiceType serviceType)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(serviceType);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceType);
+        }
+          
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+        }
+    }
+}
