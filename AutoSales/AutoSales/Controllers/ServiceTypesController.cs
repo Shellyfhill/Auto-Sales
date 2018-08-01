6f52b134202caf04303437353e82e854011ef3bf
Shellyfhill <shellyfhill@gmail.com>
2018-08-01 19:05:52 -0400

    Created Edit and Delete Views

--- AutoSales/AutoSales/Controllers/ServiceTypesController.cs
+++ AutoSales/AutoSales/Controllers/ServiceTypesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoSales.Data;
 using AutoSales.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoSales.Controllers
 {
@@ -42,6 +43,80 @@ namespace AutoSales.Controllers
             return View(serviceType);
         }
           
+        //Details : ServiceTypes/Details/1
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
+        }
+
+        //Edit : ServiceTypes/Details/1
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, ServiceType serviceType)
+        {
+            if (id != serviceType.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Update(serviceType);
+                await _db.SaveChangesAsync();
+
+               return RedirectToAction(nameof(Index));
+            }
+            return View(serviceType);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveServiceType(int id)
+        {
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            _db.ServiceTypes.Remove(serviceType);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Delete : ServiceTypes/Details/1
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
+        }
+
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
