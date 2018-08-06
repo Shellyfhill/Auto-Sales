a03160376912f5a9f007423359cb9c35c2f830cb
Shellyfhill <shellyfhill@gmail.com>
2018-08-05 22:23:38 -0400

    Created UsersController and View

---
+++ AutoSales/AutoSales/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoSales.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoSales.Controllers
+{
+    public class UsersController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public UsersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _db.Users.ToList();
+
+            return View(users);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+        }
+    }
+}
